4a59edc74dbc8e62340606073e42aaeb2494a4ce
ThomasKaiser <github@kaiser-edv.de>
2018-10-14 17:14:01 +0200

    Minor sd-card-bench corrections

--- sd-card-bench.sh
+++ sd-card-bench.sh
@@ -67,6 +67,7 @@ if [ ${RebootCount} -lt 2 ]; then
 	reboot
 elif [ ${RebootCount} -eq 2 ]; then
 	# install time package for better output parsing
+	sleep 60
 	apt-get -qq -o Dpkg::Use-Pty=0 install time
 	DISTROID=$(lsb_release -sc)
 	(/usr/bin/time -f '\nFirst desktop install time: %e\n\n\n' apt-get -qq -o Dpkg::Use-Pty=0 install armbian-${DISTROID}-desktop) &>>/root/desktop-install.txt
@@ -85,7 +86,7 @@ elif [ ${RebootCount} -eq 3 ]; then
 	# create report using averages values from the individual tests
 
 	# uptime average:
-	AvgUptime=$(tail -n 3 /root/uptime | awk '{ sum += $1 } END { if (NR > 0) print sum / NR }')
+	AvgUptime=$(awk '{ sum += $1 } END { if (NR > 0) print sum / NR }' </root/uptime)
 	echo -e "Average time until /etc/rc.local execution: ${AvgUptime} sec ($(tr '\n' ', ' </root/uptime | sed 's/,$//'))" >/root/storage-report.txt
 	
 	# IOPS values
@@ -111,7 +112,7 @@ elif [ ${RebootCount} -eq 3 ]; then
 	AutoremoveSyncTime=$(awk -F"time: " '/Autoremove sync time/ {print $2}' </root/desktop-install.txt)
 	echo "First desktop install: $(bc <<<"${Install1stTime}+${Sync1stTime}" | cut -f1 -d.) sec" >>/root/storage-report.txt
 	echo "Desktop removal time: $(bc <<<"${AutoremoveTime}+${AutoremoveSyncTime}" | cut -f1 -d.) sec" >>/root/storage-report.txt
-	echo "Second desktop install: $(bc <<<"${Install2ndTime}+${Sync2ndTime}" | cut -f1 -d.) sec\n\n\n" >>/root/storage-report.txt
+	echo -e "Second desktop install: $(bc <<<"${Install2ndTime}+${Sync2ndTime}" | cut -f1 -d.) sec\n\n\n" >>/root/storage-report.txt
 	cat /root/iozone.txt >>/root/storage-report.txt
 	echo -e "\n\n\n" >>/root/storage-report.txt
 	cat /root/desktop-install.txt >>/root/storage-report.txt

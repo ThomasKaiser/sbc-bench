1a4e27ac3c6bffc9dcb4413bd1fa2a90031f60a6
Thomas Kaiser <ThomasKaiser@users.noreply.github.com>
2022-07-18 16:24:50 +0200

    Also create table with GB scores

--- results/geekbench-rk3588/parse-geekbench-results.sh
+++ results/geekbench-rk3588/parse-geekbench-results.sh
@@ -15,4 +15,18 @@ ParseCreepbenchResults() {
 	done
 } # ParseCreepbenchResults
 
+ParseScores() {
+	echo "## RK3588(S) Geekbench scores, on the left single scores, on the right multi"
+	echo "| Device | A76 GHz | Total | Crypto | Int | FP | Total | Crypto | Int | FP |"
+	echo "| ----: | :----: |  :----: | :----: | :----: | :----: | :----: | :----: | :----: | :----: |"
+
+	ls *.txt | sort | while read file ; do
+		DeviceName=$(sed -n 17p ${file} | sed 's/rockchip //' | sed 's/  */ /g')
+		Scores="$(grep " Score" ${file} | cut -c26- | tail -n8 | tr "\n" "|" | sed 's/  */ /g')"
+		GHz=$(grep Cluster ${file} | cut -c19- | tail -n1 | awk -F" " '{print $4}')
+		echo "| [\`${DeviceName}\`](${file}) | ${GHz} | ${Scores}"
+	done
+} # ParseScores
+
 ParseCreepbenchResults >results.md
+ParseScores >scores.md
